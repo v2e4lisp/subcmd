2bae6f23b5c2bb4486f2cac19ac9214b2acb912e
wenjun.yan <mylastnameisyan@gmail.com>
2015-05-04 22:17:54 +0900

    add Name(), Setup(), Restore()

--- subcmd.go
+++ subcmd.go
@@ -3,12 +3,35 @@ package subcmd
 import "os"
 
 var (
-	Name = ""
+	name = ""
 )
 
 func init() {
+	Setup()
+}
+
+// Extract and remove sub-command from os.Args.
+// It's automatically called through init()
+func Setup() {
 	if len(os.Args) > 1 && os.Args[1][0] != '-' {
-		Name = os.Args[1]
+		name = os.Args[1]
 		os.Args = append(os.Args[:1], os.Args[2:]...)
 	}
+}
+
+// Get sub-command name. Default sub-command name is ""
+// Return default if os.Args has only one element OR
+// os.Args[1] is a flag or option(string starting with a "-")
+func Name() string {
+	return name
+}
+
+// Restore the sub-command to os.Args and reset sub-command to ""
+func Restore() {
+	if name == "" {
+		return
+	}
+
+	os.Args = append(os.Args[:1], append([]string{name}, os.Args[1:]...)...)
+	name = ""
 }
